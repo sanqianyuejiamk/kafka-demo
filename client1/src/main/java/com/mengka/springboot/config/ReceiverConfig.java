17a20cc3c33208035ee7b5adce7445ab4a7baa32
hyy044101331 <934734735@qq.com>
2018-03-26 14:04:59 +0800

    [server.properties] group.max.session.timeout.ms=3600000

--- client1/src/main/java/com/mengka/springboot/config/ReceiverConfig.java
+++ client1/src/main/java/com/mengka/springboot/config/ReceiverConfig.java
@@ -41,6 +41,7 @@ public class ReceiverConfig {
         // to the Kakfa cluster
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
                 bootstrapServers);
+//        props.put("zookeeper.connect","192.168.1.111:2181");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
                 StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
@@ -50,10 +51,10 @@ public class ReceiverConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, consumegroupId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 10000);
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 15000);
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 100000);
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 150000);
         props.put(ConsumerConfig.CONNECTIONS_MAX_IDLE_MS_CONFIG, 60000);
-        props.put(ConsumerConfig.RETRY_BACKOFF_MS_CONFIG, 10000);
+        props.put(ConsumerConfig.RETRY_BACKOFF_MS_CONFIG, 100000);
         log.info("kafka Receiver consumerConfigs = " + JSON.toJSONString(props));
         return props;
     }
